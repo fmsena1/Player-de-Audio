1825073b848915e172593cf0f05cb24d4ddaa484
Filipe Magalhães Sena <filipemagalhaessena@gmail.com>
2022-03-12 13:58:24 -0300

    1

---
+++ base/files/script.js
@@ -0,0 +1,28 @@
+window.musica =[
+
+    {
+        title:"True Love",
+        artist:"SOJA",
+        cover:"Soja.webp",
+        fileMusic:""
+
+    },
+
+    {
+        title:"Shape of you",
+        artist:"Ed Sheeran",
+        cover:"ed.jpg",
+        fileMusic:""
+
+    },
+
+    {
+        title:"Anjos",
+        artist:"O Rappa",
+        cover:"rappa.jpg",
+        fileMusic:""
+
+    }
+    
+]
+
